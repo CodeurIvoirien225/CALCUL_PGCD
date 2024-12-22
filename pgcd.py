c2d2baef947442cf4b9f740237e806823cdcad21
CodeurIvoirien225 <koneelabass1@gmail.com>
2024-12-22 08:35:19 +0000

    Ajout d'une boucle pour permettre à l'utilisateur d'effectuer plusieurs calculs de PGCD.

--- pgcd.py
+++ pgcd.py
@@ -1,15 +1,26 @@
-def pgcd_euclide(a, b):
-    while b != 0:
+# fonction pour calculer le pgcd
+def calcul_pgcd(a,b):
+    while b !=0:
         r = a % b
         a = b
         b = r
-    return a
 
+        return a
+    
 
-a = int(input("Entrez le premier nombre 'a' :"))
-b = int(input("Entrez le deuxième nombre 'b' :"))
+try:
+    while True:
+        # Entrer des nombres
+        a = int(input("Entrez le premier nombre (entier positif) :"))
+        b = int(input("Entrez le deuxième nombre (entier positif) :"))
 
-result = pgcd_euclide(a,b) 
+        if a <= 0 or b <=0:
+            raise ValueError("Les nombres doivent être des entiers positifs non nuls. Fin du programme.")
+        
+        # calcul et affichage du résultat
+        result = calcul_pgcd(a,b)
+        print(f'Le PGCD de {a} et {b} = {result}')
 
-print(f"Le PGCD est : {result}") 
+except ValueError as e:
+    print(f"Erreur : {e}")         
 
